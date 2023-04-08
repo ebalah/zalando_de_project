8aaa7e628493f9e31385f01faffc6336659813a5
ILIAS BALAH <dsicped@alamana.org.ma>
2023-04-08 21:12:20 +0000

    Change the package name to zalando_de

--- package/scrape/__init__.py
+++
@@ -1,3 +0,0 @@
-from package.scrape.main import Scrapper
-from package.scrape.units.article import ArticleScrapper
-from package.scrape.commun.assistants import ScrappingAssistant
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrppr_zalando_de',
-    version='0.1.0',
+    version='0.1.1',
     author='Ilias Balah',
     packages=find_packages()
 )
