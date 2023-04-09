45f714e458242d0b3b45aa9020ada2044f74b55e
ILIAS BALAH <dsicped@alamana.org.ma>
2023-04-09 00:10:27 +0000

    Add additional package's description in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,36 @@
-from setuptools import setup, find_packages
+import setuptools
+from pip._internal.req import parse_requirements
 
 
+# Parse the requirements file
+install_reqs = [str(ir.requirement)
+                for ir in parse_requirements('requirements.txt', session='hack')]
 
-setup(
-    name='scrppr_zalando_de',
+
+# Read the contents of the README file
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name='zalando_de_scraper',
     version='0.1.1',
+    description='A python package to scrape data from `zalando.de` website',
+    long_description=long_description,
     author='Ilias Balah',
-    packages=find_packages()
+    author_email='ibalah45@gmail.com',
+    url='https://github.com/ebalah/zalando_de_project',
+    packages=setuptools.find_packages(),
+    install_requires=install_reqs,
+    classifiers=[
+        'Intended Audience :: Upwork Client',
+        'Programming Language :: Python :: 3.10'
+    ],
+    entry_points={
+        'console_scripts': [
+            'sc_script=zalando_de.main:scrape'
+        ]
+    },
+    python_requires='>=3.6, <4',
+    include_package_data=True,
 )
