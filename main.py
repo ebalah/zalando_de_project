b61ccedc425547c09cdd19d06548b5efde7dfb85
Ilias Balah <ibalah45@gmail.com>
2023-04-13 13:34:31 +0000

    Add customized exceptions to handle browser closing and internet disconnection

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import platform
 import argparse
 import traceback
 import time
 import os
 
+import zalando_de
 from zalando_de.scrape.main import Scraper
 from zalando_de.scrape.commun.assistants import ScraperAssistant
+from zalando_de.scrape.commun.exceptions import (BrowserAlreadyClosedException,
+                                                 UnableToConnectException)
 from zalando_de.utils.logging import Logger
 from zalando_de.utils.helpers import create_directory
 
@@ -43,16 +47,10 @@ def parse_arguments():
 
 def run():
 
-    
-    # NOTE : Added only for testing
-    # LINKS = ['https://en.zalando.de/pier-one-shirt-dark-green-pi922d09r-m11.html',
-    #          'https://en.zalando.de/sir-raymond-tailor-shirt-khaki-sic22d03i-n11.html',
-    #          'https://en.zalando.de/sir-raymond-tailor-shirt-navy-bordeaux-sic22d03t-k12.html',
-    #          'https://en.zalando.de/sir-raymond-tailor-shirt-khaki-sic22d03i-n11.html',
-    #          'https://en.zalando.de/olymp-no-six-formal-shirt-bleu-ol022d04g-k11.html',
-    #          'https://en.zalando.de/pier-one-2pack-formal-shirt-whitelight-blue-pi922d05k-a11.html',
-    #          'https://en.zalando.de/next-2-pack-formal-shirt-off-white-nx322d0zp-a11.html',
-    #          'https://en.zalando.de/sir-raymond-tailor-shirt-navy-bordeaux-sic22d03t-k12.html']
+    print("Script started ...")
+    print()
+    print("Python       : {}".format(platform.python_version()))
+    print("Zalando-de   : {}".format(zalando_de.__version__))
 
     args = parse_arguments()
 
@@ -74,10 +72,12 @@ def run():
     create_directory(f"{output_dir}/logs", truncate_output)
     
     data_output_dir = f"{output_dir}/data"
-    print("Outputing data to {}".format(data_output_dir))
     
     log_output_file = f"{output_dir}/logs/logging.log"
-    print("Logging to {}".format(log_output_file))
+
+    print()
+    print("Output directory : {}".format(data_output_dir))
+    print("Logs destination :  {}".format(log_output_file))
 
     # Define the logger
     logger = Logger(log_output_file, args.log_level)
@@ -89,9 +89,6 @@ def run():
 
             try:
                 scraper = Scraper(assistant=assistant, out=data_output_dir)
-                #### Testing single article scraping. #####################
-                # scraper.scrape('single', LINKS) 
-                ###########################################################
                 scraper.scrape()
                 logger.info("Processing finished successfully.",
                             _lbr=True, _rbr=True)
@@ -99,24 +96,26 @@ def run():
                 
             except KeyboardInterrupt:
                 logger.error("Processing forcibly stopped using "
-                            "keyboard : Ctrl+C",
+                            "keyboard : Ctrl + C",
                             _lbr=True, _rbr=True)
                 logger.error(traceback.format_exc(), show_details=False)
                 break
                 
-            except Exception as e:
-                if not (hasattr(e, 'msg__browser_closed_forcibly')
-                        or hasattr(e, 'msg__internet_disconnected')):
-                    # This excepts the errors logging in case of one
-                    # of BROWSER_CLOSED_EXCEPTIONS is catched.
-                    logger.error("Processing Failed with an Error :",
-                                _lbr=True, _rbr=True)
-                    logger.error(traceback.format_exc(), show_details=False)
+            except (BrowserAlreadyClosedException,
+                    UnableToConnectException) as exc:
+                logger.error("Processing Failed with an Error :",
+                            _lbr=True, _rbr=True)
+                exc.err()
+                # Re-try
+                time.sleep(2)
+                if not input(continuing_msg).lower().startswith('y'):
                     break
-                time.sleep(5)
-                if input(continuing_msg).lower().startswith('y'):
-                    continue
 
+            except BaseException as exc:
+                logger.error("Unknown exception : ",
+                            _lbr=True, _rbr=True)
+                logger.error(traceback.format_exc(), show_details=False)
+                break
 
 if __name__ == '__main__':
     run()
