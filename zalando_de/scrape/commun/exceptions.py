85805f10013eb326759eebaa2993abcd752d374d
Ilias Balah <ibalah45@gmail.com>
2023-04-13 13:20:18 +0000

    Add customized exceptions

---
+++ zalando_de/scrape/commun/exceptions.py
@@ -0,0 +1,65 @@
+import traceback
+
+class ZalandoException(Exception):
+
+    def __init__(self, msg, exc_error, logger=None) -> None:
+        self.exc_msg = msg
+        self.exc_error = exc_error
+        self._logger = logger
+        super().__init__(self.exc_msg)
+
+    def _trace(self):
+        return traceback.format_exc()
+    
+    def dbg(self):
+        if self._logger:
+            self._logger.warn(self.exc_msg)
+        return self
+    
+    def err(self):
+        if self._logger:
+            self._logger.error(self._trace(), show_details=False)
+        return self
+    
+    def log(self):
+        if self._logger:
+            self._logger.warn(self.exc_msg, _rbr=True)
+            self._logger.error(self._trace(), show_details=False)
+        return self
+
+
+
+class UnableToOpenNewTabException(ZalandoException):
+    
+    def __init__(self, msg, *args, **kwargs) -> None:
+        super().__init__("Failed to open the new tab : "
+                         + msg, *args, **kwargs)
+
+
+class UnableToCloseNewTabException(ZalandoException):
+    
+    def __init__(self, msg, *args, **kwargs) -> None:
+        super().__init__("Failed to close the new tab : "
+                         + msg, *args, **kwargs)
+
+
+class ArticleProcessingException(ZalandoException):
+    
+    def __init__(self, msg, *args, **kwargs) -> None:
+        super().__init__("Failed to process the article : "
+                         + msg, *args, **kwargs)
+        
+
+        
+class UnableToConnectException(ZalandoException):
+    
+    def __init__(self, msg, *args, **kwargs) -> None:
+        super().__init__("Failed to conncet to Internet : "
+                         + msg, *args, **kwargs)
+        
+        
+class BrowserAlreadyClosedException(ZalandoException):
+    
+    def __init__(self, msg, *args, **kwargs) -> None:
+        super().__init__("Failed to continue : "
+                         + msg, *args, **kwargs)
